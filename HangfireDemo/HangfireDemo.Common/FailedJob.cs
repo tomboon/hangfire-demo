2208db72bc3472c12d7412527601f011022bd4e0
Roy <roy.plu@envistaco.com>
2022-08-22 12:49:21 +0200

    Added FailedJob

---
+++ HangfireDemo/HangfireDemo.Common/FailedJob.cs
@@ -0,0 +1,10 @@
+﻿namespace HangfireDemo.Common
+{
+    public class FailedJob
+    {
+        public async Task Execute()
+        {
+            throw new Exception("This job failed");
+        }
+    }
+}
--- HangfireDemo/HangfireDemo/Controllers/JobController.cs
+++ HangfireDemo/HangfireDemo/Controllers/JobController.cs
@@ -8,10 +8,18 @@ namespace HangfireDemo.Controllers
     [Route("[controller]")]
     public class JobController : ControllerBase
     {
-        [HttpPost(Name = "StartExampleJob")]
+        [HttpPost("StartExampleJob")]
         public IActionResult StartExampleJob([FromBody] int delayInSeconds)
         {
             BackgroundJob.Enqueue<ExampleJob>(job => job.Execute(new ExampleJobParameters { DelayInSeconds = delayInSeconds, Value = "whatever" }));
+            return Ok();
+        }
+
+        [HttpPost("StartFailedJob")]
+        [AutomaticRetry(Attempts = 2)]
+        public IActionResult StartFailedJob()
+        {
+            BackgroundJob.Enqueue<FailedJob>(job => job.Execute());
             return Ok();
         }
     }

b799579d99f00ed4c1955d1dabf249e8d519c3ee
Tom Boon <tom.boon@envistaco.com>
2022-08-22 11:32:14 +0200

    add webapi and console

---
+++ HangfireDemo/HangfireDemo.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hangfire;
+
+//Hangfire
+GlobalConfiguration.Configuration.UseSqlServerStorage(@"Server=(local);Data Source=.;initial catalog=HangfireDemo;Integrated Security=SSPI;Application Name=HangfireDemo");
+
+using (var server = new BackgroundJobServer())
+{
+    Console.WriteLine("Hangfire Server started. Press any key to exit...");
+    Console.ReadKey();
+}
